775b2638efca29d58f3dfe95eafa561dd9497070
Wilson <mlavwilson@mail.com>
2025-03-20 10:09:38 -0400

    Added GraphQL to simplify the number of needed rest controllers

---
+++ src/main/java/io/github/followsclosley/brick/web/controller/graphql/LegoCollectionGraph.java
@@ -0,0 +1,25 @@
+package io.github.followsclosley.brick.web.controller.graphql;
+
+import io.github.followsclosley.brick.data.entity.LegoCollection;
+import io.github.followsclosley.brick.data.repository.LegoCollectionRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class LegoCollectionGraph{
+
+    private final LegoCollectionRepository repository;
+
+    @QueryMapping
+    public List<LegoCollection> legoCollectionList(@Argument Integer page) {
+        return repository.findAll();
+    }
+
+}
